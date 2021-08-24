2c569d008fa66ba4bd84d39418ee251cb6460aa0
PhuocMai <maiphuochd@gmail.com>
2021-08-24 17:59:12 +0700

    support pushgateway server with basic authentication
    - support pushgateway server with basic authentication
    - update readme

--- README.md
+++ README.md
@@ -1,21 +1,28 @@
 # Introduction
-This service for collecting metrics from internal machine / exporter push to Pushgateway.
+This service for collecting metrics from machine/exporter in internal network and pushing to Pushgateway.
 # Getting started
+## Overview:
+![GitHub Logo](/push-service.png)
 
-Some envionments need to config:
-- SCRAP_INTERVAL: scrap time interval ,                                                 (default: 30s)
-- PUSHGATEWAY_SERVER = pushgateway server url                                           (default: 'http://host.docker.internal:9091')
-- EXPORTER_ENDPOINT: array exporter endpoint with format endpoint_url|job|instance      (default: 'http://host.docker.internal:9182/metrics|job1|instance1')
-> For example EXPORTER_ENDPOINT=http://host.docker.internal:9182/metrics|window_exporter_job|my_computer,http://host.docker.internal:9090/metrics|prometheus_job|prometheus\
-> Note that all label will overide the the label in pushgateway
+## Some envionments need to config:
+| Enviroment variable | Description | Default value|
+| ------ | ------ |-----|
+|SCRAP_INTERVAL|scrap time interval|  30s |
+|PUSHGATEWAY_SERVER|Pushgateway server url|http://host.docker.internal:9091|
+|BASIC_AUTH_USERNAME|Pushgateway server basic authentication username|None|
+|BASIC_AUTH_PASSWD|Pushgateway server basic authentication password|None|
+|EXPORTER_ENDPOINT|array exporter endpoint with format ```endpoint_url|job|instance``` separate by comma ',' |http://host.docker.internal:9182/metrics|job1|instance1|
+
+> For example:\
+> EXPORTER_ENDPOINT=http://host.docker.internal:9182/metrics|window_exporter_job|my_computer,http://host.docker.internal:9090/metrics|prometheus_job|prometheus\
 ## Run by docker
 ```
-docker pull 2clicksolutions/pushservice:0.1
-docker run -e pushservice.env 2clicksolutions/pushservice:0.1
+docker pull 2clicksolutions/pushservice:0.2
+docker run -e pushservice.env 2clicksolutions/pushservice:0.2
 ```
 ## Run by docker-compose
 ```
-docker pull 2clicksolutions/pushservice:0.1
+docker pull 2clicksolutions/pushservice:0.2
 docker-compose up -d
 
 ```
--- push_service.py
+++ push_service.py
@@ -4,33 +4,38 @@ import os
 import logging
 import validators
 import sys
+from requests.auth import HTTPBasicAuth
 
 SCRAP_INTERVAL = os.getenv('SCRAP_INTERVAL', 30)
 EXPORTER_ENDPOINT = os.getenv('EXPORTER_ENDPOINT', 'http://host.docker.internal:9182/metrics|job1|instance1')
 PUSHGATEWAY_SERVER = os.getenv('PUSHGATEWAY_SERVER', 'http://host.docker.internal:9091')
+BASIC_AUTH_USERNAME = os.getenv('BASIC_AUTH_USERNAME', '')
+BASIC_AUTH_PASSWD = os.getenv('BASIC_AUTH_PASSWD', '')
 
 logging.basicConfig(level=logging.INFO,format="%(asctime)s - %(levelname)s - %(message)s")
 def main():
-    try:
-        requests.get(PUSHGATEWAY_SERVER + '/-/healthy').status_code == 200
-    except:
-        logging.error("Can't connect to Pushgateway {e}".format(e=PUSHGATEWAY_SERVER))
-        exit(0)
-    logging.info("Connected to Pushgateway")
     while True:
+        if requests.get(PUSHGATEWAY_SERVER + '/-/ready',auth=HTTPBasicAuth(BASIC_AUTH_USERNAME, BASIC_AUTH_PASSWD)).status_code == 200:
+            logging.info("Pushgateway is ready to serve traffic")
+        elif requests.get(PUSHGATEWAY_SERVER + '/-/ready',auth=HTTPBasicAuth(BASIC_AUTH_USERNAME, BASIC_AUTH_PASSWD)).status_code == 401:
+            logging.error("Unauthorization")
+            exit(0)
+        else:
+            logging.error("Can't connect to Pushgateway.")
+            exit(0)
         for e in EXPORTER_ENDPOINT.split(','):
             endpoint, job_name, instance_name = e.split('|')
             if ((validators.url(endpoint)) == True):
                 try:
                     requests.get(endpoint).status_code == 200
                 except:
-                    logging.error("Can't get metric from {e}".format(e=endpoint))
+                    logging.error("Can't get metric from {e}.".format(e=endpoint))
                     continue
                 resp = requests.get(endpoint)
                 response = requests.post('{p}/metrics/job/{j}/instance/{i}'.format(p=PUSHGATEWAY_SERVER, j=job_name, i=instance_name), data=resp.text)
-                logging.info("Get metrics from {e} and push to {p}".format(e=endpoint,p=PUSHGATEWAY_SERVER))
+                logging.info("Get metrics from {e} and push to Pushgateway.".format(e=endpoint))
             else:
-                logging.error("Invalid exporter endpoint url {e}".format(e=EXPORTER_ENDPOINT))
+                logging.error("Invalid exporter endpoint url {e}.".format(e=EXPORTER_ENDPOINT))
         time.sleep(int(SCRAP_INTERVAL))
 if __name__ == '__main__':
     main()
